551988fcaa8e84e3cfd13e7ee1ba806bd4c430e6
Michał Chałubiec <michal.chalubiec@outlook.com>
2022-05-24 01:06:44 +0200

    added utility get input

---
+++ BankSystem/Utility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankSystem
+{
+    public static class Utility
+    {
+        public static string GetUserInput(string requestContent)
+        {
+            Console.Write($"Enter {requestContent}");
+            return Console.ReadLine();
+        }
+    }
+}
